3ba3423d3bab990b8647977807d421c3531f6f6c
Tal Oz <taloz@mobileye.com>
2021-06-06 11:08:50 +0300

    Add calculator functions for addition and subtraction.

---
+++ calculator.py
@@ -0,0 +1,20 @@
+"""
+Calculator lubrary contianing basic math operations.
+"""
+
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
+def main():
+    print('Whatever I do what I want')
+    return None
+
+
+if __name__ == "__main__":
+    main()
